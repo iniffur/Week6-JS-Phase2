3d17ad629bfb7325b1cc9d8ead9cbc44966e319a
iniffur <jamesruffini97@gmail.com>
2022-09-30 13:21:04 +0100

    ExpressJS example

--- CandySearchFunction/github.test.js
+++ CandySearchFunction/github.test.js
@@ -4,20 +4,20 @@ describe('Github', () => {
   it('fetches data', () => {
       const mockedAPI = {
         fetchRepositoryInfo: (repoName, callback) => {
-              callback({
-								name: 'sinatra/sinatra',
-								description: 'Some fake description'
-							});
-          }
-      };
+            callback({
+							name: 'sinatra/sinatra',
+							description: 'Some fake description'
+						});
+        	}
+        };
 
-			const github = new Github(mockedAPI);
+		const github = new Github(mockedAPI);
 
-			github.fetch('sinatra/sinatra');
+		github.fetch('sinatra/sinatra');
 
-			expect(github.getRepoData()).toEqual({
-				name: 'sinatra/sinatra',
-				description: 'Some fake description'
-			});
+		expect(github.getRepoData()).toEqual({
+			name: 'sinatra/sinatra',
+			description: 'Some fake description'
+		});
   });
 });
